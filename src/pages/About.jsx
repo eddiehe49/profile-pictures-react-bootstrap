c314024425af6df33d338bc8318c4dae9249b079
Eddie-He-090 <2021022249@m.scnu.edu.cn>
2022-05-08 11:30:28 +0800

    Update Waline.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,42 +1,19 @@
 import React from "react";
 import '../App.css';
 import logo from "../logo.svg";
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useContext } from "react";
 import waline from '../services/Waline.js';
-import { Button, Offcanvas } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { tipsModal } from "../App";
 
 
-function OffCanvasExample({ name, ...props }) {
-    const [offcanvasShow, setOffcanvasShow] = useState(false);
-    return (
-        <div style={{ padding: "2% 0 0 0" }}>
-            <Button variant="primary" onClick={() => { setOffcanvasShow(true) }} className="me-2">
-                What are avatars?
-            </Button>
-            <Offcanvas show={offcanvasShow} onHide={() => { setOffcanvasShow(false) }} {...props} className="componentsDarkMode">
-                <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>What are avatars?</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                    Check out this <a href="https://web.okjike.com/u/60001f57-6fec-4ad1-963b-51ebdf913f8c" target="_blank" rel="noopener noreferrer" style={{ color: "#0d6efd" }}>
-                        site
-                    </a> on your desktop browser.
-                    <br />
-                    Do you see the round picture in the top-left corner of every post?
-                    <br />
-                    That's my avatar!
-                </Offcanvas.Body>
-            </Offcanvas>
-        </div>
-    );
-}
-
 function About(params) {
     const { setTipsModalShow } = useContext(tipsModal)
+
     useEffect(() => {
         waline.update()
     }, [])
+
     return (
         <div className="App">
             <header className="App-header">
