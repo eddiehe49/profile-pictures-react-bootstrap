a9e705609b920a0f12baca6da5a10cc1eaba8cfb
Eddie-He-090 <2021022249@m.scnu.edu.cn>
2022-07-04 16:55:56 +0800

    Add Krates information in .env

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const jsonplaceholderApiClient = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/eddiehe49/profile-pictures',
+    baseURL: "https://my-json-server.typicode.com/eddiehe49/profile-pictures",
     withCredentials: false,
     headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json"
     }
 })
 const jsonbinApiClient = axios.create({
@@ -13,37 +13,37 @@ const jsonbinApiClient = axios.create({
     withCredentials: false,
     headers: {
         "X-Master-Key": "$2b$10$Dr3C03m96HY3V014" + process.env.REACT_APP_X_Master_Key,
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json"
     }
 })
 const kratesApiClient = axios.create({
-    baseURL: "https://krat.es/" + process.env.REACT_APP_Krate_ID,
+    baseURL: "https://krat.es/" + process.env.REACT_APP_Krates_ID,
     withCredentials: false,
     headers: {
-        "x-api-key": process.env.REACT_APP_x_api_key,
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
+        "x-api-key": process.env.REACT_APP_Krates_x_api_key,
+        Accept: "application/json",
+        "Content-Type": "application/json"
     }
 })
 
 const getJsonplaceholderProfilePicturesJson = () => {
-    return jsonplaceholderApiClient.get('/profilePictures')
+    return jsonplaceholderApiClient.get("/profilePictures")
 }
 const patchJsonplaceholderProfilePicturesJson = (id, profilePictures) => {
-    return jsonplaceholderApiClient.patch('/profilePictures/' + id, profilePictures)
+    return jsonplaceholderApiClient.patch("/profilePictures/" + id, profilePictures)
 }
 const getJsonbinProfilePicturesJson = () => {
-    return jsonbinApiClient.get('/latest')
+    return jsonbinApiClient.get("/latest")
 }
 const putJsonbinProfilePicturesJson = (profilePictures) => {
-    return jsonbinApiClient.put('', profilePictures)
+    return jsonbinApiClient.put("", profilePictures)
 }
 const getKratesProfilePicturesJson = () => {
-    return kratesApiClient.get("/record/624c33c65a015dc4d2223b6e")
+    return kratesApiClient.get("/record/" + process.env.REACT_APP_Krates_record)
 }
 const putKratesProfilePicturesJson = (profilePictures) => {
-    return kratesApiClient.put('/624c33c65a015dc4d2223b6e', profilePictures)
+    return kratesApiClient.put("/" + process.env.REACT_APP_Krates_record, profilePictures)
 };
 
 const Service = { getJsonplaceholderProfilePicturesJson, patchJsonplaceholderProfilePicturesJson, getJsonbinProfilePicturesJson, putJsonbinProfilePicturesJson, getKratesProfilePicturesJson, putKratesProfilePicturesJson }
