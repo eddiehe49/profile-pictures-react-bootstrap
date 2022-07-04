1e75038570433a08957276c803251bd68ad8d5fe
Eddie-He-090 <2021022249@m.scnu.edu.cn>
2022-07-04 17:30:01 +0800

    Change " to '

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,34 +1,34 @@
 import axios from "axios";
 
 const jsonplaceholderApiClient = axios.create({
-    baseURL: "https://my-json-server.typicode.com/eddiehe49/profile-pictures",
+    baseURL: 'https://my-json-server.typicode.com/eddiehe49/profile-pictures',
     withCredentials: false,
     headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
     }
 })
 const kratesApiClient = axios.create({
-    baseURL: "https://krat.es/" + process.env.REACT_APP_Krates_ID,
+    baseURL: 'https://krat.es/' + process.env.REACT_APP_Krates_ID,
     withCredentials: false,
     headers: {
-        "x-api-key": process.env.REACT_APP_Krates_x_api_key,
-        Accept: "application/json",
-        "Content-Type": "application/json"
+        'x-api-key': process.env.REACT_APP_Krates_x_api_key,
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
     }
 })
 
 const getJsonplaceholderProfilePicturesJson = () => {
-    return jsonplaceholderApiClient.get("/profilePictures")
+    return jsonplaceholderApiClient.get('/profilePictures')
 }
 const patchJsonplaceholderProfilePicturesJson = (id, profilePictures) => {
-    return jsonplaceholderApiClient.patch("/profilePictures/" + id, profilePictures)
+    return jsonplaceholderApiClient.patch('/profilePictures/' + id, profilePictures)
 }
 const getKratesProfilePicturesJson = () => {
-    return kratesApiClient.get("/record/" + process.env.REACT_APP_Krates_record)
+    return kratesApiClient.get('/record/' + process.env.REACT_APP_Krates_record)
 }
 const putKratesProfilePicturesJson = (profilePictures) => {
-    return kratesApiClient.put("/" + process.env.REACT_APP_Krates_record, profilePictures)
+    return kratesApiClient.put('/' + process.env.REACT_APP_Krates_record, profilePictures)
 };
 
 const Service = { getJsonplaceholderProfilePicturesJson, patchJsonplaceholderProfilePicturesJson, getKratesProfilePicturesJson, putKratesProfilePicturesJson }
